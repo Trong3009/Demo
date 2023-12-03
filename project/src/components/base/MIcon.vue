de526eabb7cdfe5723cebeccb59c137a861c653e
Trong3009 <quytrong3009@gmail.com>
2023-12-03 17:32:37 +0700

    `create new file`

---
+++ project/src/components/base/MIcon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="icon">
+        <div class="wrapper-icon">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>

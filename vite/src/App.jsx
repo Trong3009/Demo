ed526ac24c107697ce6a40fc78584fa2a0a84784
Trong3009 <quytrong3009@gmail.com>
2024-01-02 17:36:07 +0700

    update Light and dark Theme

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,11 +1,28 @@
 
-import { Button } from '@mui/material'
+import {
+    Button, Typography,useColorScheme  } from '@mui/material'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+    
 function App() {
 
   return (
     <>
+    <ModeToggle></ModeToggle>
+    <hr />
     <div>trongnguyenquy</div>
+    <Typography variant="body2" color="text.secondary">sdasdasd</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>

0fe5b935c2f20acb06c3c727c6b3b61e5113b14d
Trong3009 <quytrong3009@gmail.com>
2024-01-08 10:11:06 +0700

    Refactor project Structure

---
+++ vite/src/pages/Boards/BoardsBar/index.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material"
+
+function BoardsBar
+() {
+  return (
+    <Box sx={{
+        backgroundColor:'primary.dark',
+        width:'100%',
+        height:'56px',
+        display:'flex',
+        alignItems:'center'
+      }}>
+        minnd
+      </Box>
+  )
+}
+
+export default BoardsBar
